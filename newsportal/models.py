1086b8813f85bca648a250b59201cf8f44784364
DenSznk <densznk@gmail.com>
2022-07-14 21:48:31 +0400

    Initial

---
+++ newsportal/models.py
@@ -0,0 +1,67 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models import Sum, Max
+from newsportal.resources import CATEGORY_NAME
+
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0)
+
+    def update_rating(self):
+        rating_post_author = self.post_set.all().aggregate(sum=Sum('rating') * 3)['sum']
+        rating_comment = self.user.comment_set.all().aggregate(sum=Sum('comment_rating'))['sum']
+        rating_comment_post = Post.objects.filter(author=self).aggregate(sum=Sum('rating'))['sum']
+        self.sum_rating = rating_post_author + rating_comment + rating_comment_post
+        self.save()
+
+        # return f'{rating_post_author}  {rating_comment}   {rating_comment_post}'
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=155, unique=True)
+
+
+class Post(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    choice_category = models.CharField(max_length=2, choices=CATEGORY_NAME)
+    date_time_auto = models.DateTimeField(auto_now_add=True)
+    category = models.ManyToManyField(Category, through='PostCategory')
+    header_news = models.CharField(max_length=155)
+    post_text = models.TextField()
+    rating = models.IntegerField(default=0)
+
+    def add_like(self):
+        self.rating += 1
+        self.save()
+
+    def add_dislike(self):
+        self.rating -= 1
+        self.save()
+
+    def get_preview(self):
+        preview = f"{self.post_text[:124]}"
+        if len(preview) > 124:
+            preview += '...'
+        return preview
+
+
+class Comment(models.Model):
+    user_one_to_many = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_one_to_many = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment_text = models.TextField()
+    comment_date = models.DateTimeField(auto_now_add=True)
+    comment_rating = models.IntegerField(default=0)
+
+    def add_like(self):
+        self.comment_rating += 1
+        self.save()
+
+    def add_dislike(self):
+        self.comment_rating -= 1
+        self.save()
+
+
+class PostCategory(models.Model):
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
---
+++ newsportal/resources.py
@@ -0,0 +1,7 @@
+news = 'NE'
+article = 'AR'
+CATEGORY_NAME = [
+    (article, 'Article'),
+    (news, 'News')
+
+]
